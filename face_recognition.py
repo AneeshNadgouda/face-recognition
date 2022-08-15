600dedeee0e0ac67b57c2338b00b9d0af4671459
Aneesh <aneeshnadgouda581@gmail.com>
2022-08-15 09:10:15 -0400

    changed parameters for face cascade scaling

--- face_recognition.py
+++ face_recognition.py
@@ -7,7 +7,7 @@ cap = cv2.VideoCapture(0)
 while True:
     flag, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     for(x, y, w, h) in faces:
         cv2.rectangle(frame, (x,y), (x+w, y+h), (255, 0, 0), 2)
     cv2.imshow('frame', frame)
